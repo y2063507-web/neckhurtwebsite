823abe3d5a9f49acdcab3aaf17505a8ccb962d98
y2063507-web <y2063507@gmail.com>
2026-08-07 18:23:43 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -86,4 +86,6 @@ io.on("connection", (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`pnig backend running on port ${PORT}`));
+server.listen(PORT, () => {
+    console.log(`pnig backend running on port ${PORT}`);
+});
